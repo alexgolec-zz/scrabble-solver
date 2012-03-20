21a9abe05b40bf82708a8e96079230ce93be1955
Alexander Golec <thejfasi@gmail.com>
2012-03-20 13:42:36 -0400

    Added pattern generation capabilities to the board.

--- board/board.py
+++ board/board.py
@@ -110,13 +110,6 @@ class BoardState:
         self.width = w
         # a map from (x, y) tuple to tile object
         self.board = defaultdict(none)
-        self.words = defaultdict(list)
-    def get_words(self, word):
-        '''
-        Returns a list of word objects corresponding to the given word.
-        note that the list may be empty.
-        '''
-        return self.words[word]
     def get_tile(self, tile):
         '''
         Returns the tile object for this position, or None is no word was
@@ -166,20 +159,78 @@ class BoardState:
             if tiles is not None:
                 words.append(''.join([t.letter for t in tiles]))
         return words
+    def __make_pattern_rec(self, direction, remaining, current, ret, visited):
+        # have we seen this word already?
+        word = tuple([w.letter for w in current])
+        if word not in visited:
+            visited.add(word)
+        else:
+            return
+        # if we've run out of words, then add
+        if remaining == 0:
+            ret.append(current)
+            return
+        # establish the frontier
+        frontier_left = (current[0].pos.x - direction.x, current[0].pos.y - direction.y)
+        frontier_right = (current[-1].pos.x + direction.x, current[-1].pos.y + direction.y)
+        # if there is already a tile on the frontier, then jump over it by
+        # recursing down until there are no more tiles
+        if frontier_left in self.board:
+            self.__make_pattern_rec(direction, remaining,
+                                    [self.board[frontier_left]]+current,
+                                    ret, visited)
+            return
+        if frontier_right in self.board:
+            self.__make_pattern_rec(direction, remaining,
+                                    current+[self.board[frontier_right]],
+                                    ret, visited)
+            return
+        # by the time we reach here, we know both sides are good to explore
+        if frontier_left in self:
+            self.__make_pattern_rec(direction, remaining - 1,
+                                    [BoardTile(frontier_left, '_')]+current,
+                                    ret, visited)
+        if frontier_right in self:
+            self.__make_pattern_rec(direction, remaining - 1,
+                                    current+[BoardTile(frontier_right, '_')],
+                                    ret, visited)
+    def make_patterns(self, pos, direction, max_tiles=7):
+        if pos not in self:
+            raise ValueError, 'invalid tile location'
+        if pos not in self.board:
+            raise ValueError, 'empty position tile'
+        ret = []
+        for remaining in xrange(0, max_tiles):
+            scr = []
+            self.__make_pattern_rec(direction, remaining, [self.board[pos]], scr, set())
+            ret.extend(scr)
+        return ret
     def __contains__(self, pos):
         return (pos[0] >= 0 and pos[0] <= self.width and
                 pos[1] >= 0 and pos[1] <= self.height)
 
-b = BoardState(15, 15)
+if __name__ == '__main__':
+    b = BoardState(15, 15)
 
-# ac
-# bd
-# e
+    # ac
+    # bd
+    # e
 
-b.board[(0, 0)] = BoardTile((0, 0), 'a')
-b.board[(0, 1)] = BoardTile((0, 1), 'b')
-b.board[(0, 2)] = BoardTile((0, 2), 'e')
-b.board[(1, 0)] = BoardTile((1, 0), 'c')
-b.board[(1, 1)] = BoardTile((1, 1), 'd')
+    b.board[(0, 0)] = BoardTile((0, 0), 'a')
+    b.board[(0, 1)] = BoardTile((0, 1), 'b')
+    b.board[(0, 2)] = BoardTile((0, 2), 'e')
+    b.board[(1, 0)] = BoardTile((1, 0), 'c')
+    b.board[(1, 1)] = BoardTile((1, 1), 'd')
 
-print b.get_intersecting_words(Word([BoardTile( (12, 12), 'a'), BoardTile( (12, 13), 'a')]))
+    for i in xrange(0, 15):
+        for j in xrange(0, 15):
+            try:
+                ret = b.make_patterns((i, j), ACROSS)
+            except ValueError:
+                pass
+            try:
+                ret = b.make_patterns((i, j), DOWN)
+            except ValueError:
+                pass
+
+    print '\n'.join([''.join([i.letter for i in j]) for j in ret])
